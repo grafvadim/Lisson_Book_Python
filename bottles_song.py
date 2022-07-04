ffae8b0e994ab3b43654428f32e9c79755e789cc
grafvadim <grafvadim@gmail.com>
2022-07-04 14:03:43 +0300

    Song of the beer

---
+++ bottles_song.py
@@ -0,0 +1,14 @@
+world = "bottles"
+for beer_num in range(99, 0, -1):
+    print(beer_num, world, "of beer on the wall.")
+    print(beer_num, world, "of beer.")
+    print("Take one down.")
+    print("Pass it around.")
+    if beer_num == 1:
+        print("No more bottles of beer on the wall.")
+    else:
+        new_num = beer_num - 1
+        if new_num == 1:
+            world = "bottle"
+        print(new_num, world, "of beer on the wall.")
+    print()
